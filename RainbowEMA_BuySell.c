32aea59288b0004cc5b58b50609a03b5d3f9a8df
Benjamin Cance <49796265+rowingdude@users.noreply.github.com>
2023-10-23 15:07:05 -0400

    Update RainbowEMA_BuySell.c

--- RainbowEMA_BuySell.c
+++ RainbowEMA_BuySell.c
@@ -5,42 +5,25 @@
 #define MAX_EMA_COUNT 5
 
 void rainbow_ema(float closing_price, float* prev_ema, float* time_interval, float* ema_values, int ema_count, bool* buy_sell) {
-    float ema_100 = prev_ema[ema_count - 1]; // the 100 EMA is the last one in the array
-    for (int i = 0; i < ema_count; i++) {
-        float k = 2.0 / (time_interval[i] + 1.0);
-        ema_values[i] = (closing_price * k) + (prev_ema[i] * (1 - k));
+    float ema_100 = prev_ema[ema_count - 1]; // The 100 EMA is the last one in the array
+    bool prev_decision = buy_sell[0];
+    for (int i = 0; i < ema_count; ++i) {
+        float inv_interval = 1.0f / (time_interval[i] + 1.0f); // Precompute the inverse for later use
+        float k = 2.0f * inv_interval;
+        float one_minus_k = 1.0f - k;
+        
+        // Calculate EMA value and update it
+        ema_values[i] = (closing_price * k) + (prev_ema[i] * one_minus_k);
         prev_ema[i] = ema_values[i];
+        
+        // Determine buy or sell signals
         if (i == ema_count - 1) {
-            // check position of 100 EMA and set buy_sell accordingly
-            if (ema_100 > ema_values[i]) {
-                buy_sell[i] = false; // sell
-            } else {
-                buy_sell[i] = true; // buy
-            }
+            buy_sell[i] = ema_100 <= ema_values[i];
         } else {
-            // check position of EMA and 100 EMA and set buy_sell accordingly
-            if (ema_100 > ema_values[i] && ema_100 > prev_ema[i]) {
-                buy_sell[i] = false; // sell
-            } else if (ema_100 < ema_values[i] && ema_100 < prev_ema[i]) {
-                buy_sell[i] = true; // buy
-            } else {
-                buy_sell[i] = buy_sell[i-1]; // maintain previous buy_sell decision
-            }
+            bool sell_condition = ema_100 > ema_values[i] && ema_100 > prev_ema[i];
+            bool buy_condition = ema_100 < ema_values[i] && ema_100 < prev_ema[i];
+            buy_sell[i] = buy_condition ? true : (sell_condition ? false : prev_decision);
+            prev_decision = buy_sell[i];
         }
     }
-}
-
-int main() {
-    float opening_price = 100.0;
-    float closing_price = 120.0;
-    float time_interval[MAX_EMA_COUNT] = {5.0, 10.0, 20.0, 50.0, 100.0}; // time intervals for each EMA
-    float prev_ema[MAX_EMA_COUNT] = {opening_price, opening_price, opening_price, opening_price, opening_price}; // initialize previous EMA values with opening price
-    float ema_values[MAX_EMA_COUNT];
-    bool buy_sell[MAX_EMA_COUNT];
-    int ema_count = 5;
-    rainbow_ema(closing_price, prev_ema, time_interval, ema_values, ema_count, buy_sell);
-    for (int i = 0; i < ema_count; i++) {
-        printf("EMA%d: %.2f, Buy/Sell: %s\n", (int)time_interval[i], ema_values[i], buy_sell[i] ? "Buy" : "Sell");
-    }
-    return 0;
 }
